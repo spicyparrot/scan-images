8cfe214420a7b9a072d08fe50852ea8e28a3e3aa
cbath <craigbath@outlook.com>
2022-11-17 13:53:00 -0500

    testing

--- scan.sh
+++ scan.sh
@@ -13,6 +13,8 @@ WHITE='\033[0m'
 
 # Files
 TAB_FILE=/tmp/table.txt
+FULL_FILE=/tmp/full.txt
+SLIM_FILE=/tmp/simple.txt
 HTML_REPORT=/tmp/trivy_image_reports.html
 TOTAL_FILE=/tmp/totals
 
@@ -28,14 +30,14 @@ for IMAGE in $IMAGES; do
   printf "${YELLOW}Scanning ${IMAGE}...\n${WHITE}"
 
   # Full formatted report
-  trivy image --format template --template "@templates/html.tpl" -o /tmp/full.txt --severity ${SEVERITIES} --ignore-unfixed ${IMAGE}
+  trivy image --format template --template "@templates/html.tpl" -o ${FULL_FILE} --severity ${SEVERITIES} --ignore-unfixed ${IMAGE}
   echo "" >> ${HTML_REPORT}
-  cat full.txt >> ${HTML_REPORT}
+  cat ${FULL_FILE}  >> ${HTML_REPORT}
 
   # Reduced report for GitHub Step Summary
-  trivy image --format template --template "@templates/html.tpl" -o /tmp/simple.txt --severity ${SEVERITIES} --ignore-unfixed ${IMAGE}
+  trivy image --format template --template "@templates/html.tpl" -o ${SLIM_FILE}  --severity ${SEVERITIES} --ignore-unfixed ${IMAGE}
   echo "" >> $GITHUB_STEP_SUMMARY
-  cat simple.txt >> $GITHUB_STEP_SUMMARY;
+  cat ${SLIM_FILE}  >> $GITHUB_STEP_SUMMARY;
 
   # Add cumulative issues
   trivy image -o ${TAB_FILE} --severity ${SEVERITIES} --ignore-unfixed ${IMAGE}
